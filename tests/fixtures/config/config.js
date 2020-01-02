bf80d023ed95ba957a0b807183e31d11684bf6ac
Metais Fabien <1566061+metaisfabien@users.noreply.github.com>
2020-01-02 16:11:28 +0000

    Test fix for ci

--- tests/fixtures/config/config.js
+++ tests/fixtures/config/config.js
@@ -34,7 +34,7 @@ export default {
   },
   mongo: {
     default: {
-      uri: 'mongodb://localhost:27017/midgar?connectTimeoutMS=1000',
+      uri: process.env.MONGO_CONNEXION_STR ? process.env.MONGO_CONNEXION_STR : 'mongodb://localhost:27017/midgar?connectTimeoutMS=1000',
       options: {
         useNewUrlParser: true,
         useUnifiedTopology: true
