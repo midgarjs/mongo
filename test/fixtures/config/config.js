4f5517ddf3b9763ce365b0efd1c82c22127d9a5e
Metais Fabien <1566061+metaisfabien@users.noreply.github.com>
2020-06-23 19:26:48 +0200

    up

--- test/fixtures/config/config.js
+++ test/fixtures/config/config.js
@@ -1,47 +1,50 @@
-
-import path from 'path'
-import os from 'os'
-import uid from 'uid-safe'
-import mkdirp from 'mkdirp'
+import path from "path";
+import os from "os";
+import uid from "uid-safe";
+import mkdirp from "mkdirp";
 
 const resolve = (p) => {
-  return path.join(__dirname, p)
-}
+  return path.join(__dirname, p);
+};
 
-function getTmpDir (name) {
-  const dirname = path.join(os.tmpdir(), uid.sync(8), name)
+function getTmpDir(name) {
+  const dirname = path.join(os.tmpdir(), uid.sync(8), name);
 
-  mkdirp.sync(dirname, { mode: parseInt('0700', 8) })
+  mkdirp.sync(dirname, { mode: parseInt("0700", 8) });
 
-  return dirname
+  return dirname;
 }
 
 const config = {
   log: {
-    dir: getTmpDir('logs'),
+    dir: getTmpDir("logs"),
     stdout: true,
-    level: 'error'
+    level: "error",
   },
-  pluginsLocalPath: resolve('../plugins'),
+  pluginsLocalPath: resolve("../plugins"),
+
   mongo: {
     default: {
-      uri: process.env.MONGO_CONNEXION_URI ? process.env.MONGO_CONNEXION_URI : 'mongodb://localhost:27017/midgar-test?connectTimeoutMS=1000',
-      options: {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }
-    }
-  }
-}
-
+      uri: process.env.MONGO_CONNEXION_URI,
+    },
+  },
+};
 
 // Set Auth options
 if (process.env.MONGO_CONNEXION_USER && process.env.MONGO_CONNEXION_PASSWORD) {
   config.mongo.default.options = {
-    auth: { authSource: 'admin' },
+    auth: { authSource: "admin" },
     user: process.env.MONGO_CONNEXION_USER,
-    pass: process.env.MONGO_CONNEXION_PASSWORD
-  }
+    pass: process.env.MONGO_CONNEXION_PASSWORD,
+  };
+}
+// Set Auth options
+if (process.env.MONGO_CONNEXION_USER && process.env.MONGO_CONNEXION_PASSWORD) {
+  config.mongo.default.options = {
+    auth: { authSource: "admin" },
+    user: process.env.MONGO_CONNEXION_USER,
+    pass: process.env.MONGO_CONNEXION_PASSWORD,
+  };
 }
 
-export default config
+export default config;
